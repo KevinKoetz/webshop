46647760e1d7a2f52dc22a4ca7af881968a54463
Zeeshan <zeeshan.iqbal@hotmail.de>
2022-01-21 15:07:53 +0100

    imported Pictures component into WebShop.tsx

--- client/src/WebShop.tsx
+++ client/src/WebShop.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import './WebShop.css';
-import {useRouteMatch, Switch, Route} from 'react-router-dom'; 
-import Picture from './components/Picture/Picture'; 
-import Pictures from './components/Pictures/Pictures'
-;
-
+import React from "react";
+import "./WebShop.css";
+import { useRouteMatch, Switch, Route } from "react-router-dom";
+import Picture from "./components/Picture/Picture";
+import Pictures from "./components/Pictures/Pictures";
 
 function WebShop() {
-
   return (
     <div className="WebShop">
-      Hello 
+      Hello
       <Switch>
-        <Route path="/" > 
-        <Pictures />
-       
+        <Route path="/">
+          <Pictures />
         </Route>
       </Switch>
     </div>
   );
 }
 
-export default WebShop; 
+export default WebShop;
