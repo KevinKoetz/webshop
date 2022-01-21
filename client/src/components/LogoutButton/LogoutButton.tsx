f3a21ccf1d3833c726e612260a987909e912da13
Kevin Kötz <8590427+KevinKoetz@users.noreply.github.com>
2022-01-21 10:42:02 +0100

    reset UserState on Logout

--- client/src/components/LogoutButton/LogoutButton.tsx
+++ client/src/components/LogoutButton/LogoutButton.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { UserContext } from '../UserContext/UserContext';
 
 const LogoutButton = ({redirectUrl = "/"}) => {
+    const {setUserState} = useContext(UserContext);
     const handleLogout = (e: React.MouseEvent<HTMLInputElement>) => {
         console.log(document.cookie);
         fetch("/logout", {method: "POST"});
+        setUserState({isAuthenticated: false, username: null})
     }
     return ( 
         <input type="button" value="Logout" onClick={handleLogout}/>
