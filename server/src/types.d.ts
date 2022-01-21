a8a4f7ec09b4dbdbe5c6f6ee3da4662deccf8646
Kevin Kötz <8590427+KevinKoetz@users.noreply.github.com>
2022-01-21 12:47:48 +0100

    Added Additional keys to User Type

--- server/src/types.d.ts
+++ server/src/types.d.ts
@@ -1,7 +1,37 @@
 declare namespace Express {
+
+  export interface ProfileDetails {
+    aboutMe?: string;
+  }
+  
+  export interface InvoiceDetails {
+    fullName: string;
+    streetWithNumber: string;
+    postCode: string;
+    country: string;
+  }
+
+  export interface PaymentDetails {
+    fullName: string;
+    bic: string;
+    iban: string;
+    bank: string;
+  }
+
+  export interface UserDetails {
+    profile?: ProfileDetails;
+    art?: string[];
+    invoice?: InvoiceDetails;
+    payment?: PaymentDetails;
+  }
   export interface User {
     id: string;
     username: string;
     password: string; //storing plain password here, do not ever Ever do this at home!!!
+    profile?: ProfileDetails;
+    art?: string[];
+    invoice?: InvoiceDetails;
+    payment?: PaymentDetails;
   }
+  
 }
