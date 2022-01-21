26769972999d02caa643666f2f3f03e1ebefa261
Zeeshan <zeeshan.iqbal@hotmail.de>
2022-01-21 10:55:21 +0100

    implemented PicturePreview component

--- client/src/components/PicturePreview/PicturePreview.jsx
+++ client/src/components/PicturePreview/PicturePreview.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React from "react";
+import "./PicturePreview.css";
+import { Link } from "react-router-dom";
 
-const PicturePreview = () => {
-    return ( 
-        <div>Picture Preview</div>
-     );
-}
- 
+const PicturePreview = ({ id, artist, price, thumbnailUrl }) => {
+  return (
+    <Link className="PicturePreview" to={`/picture/${id}`}>
+      <figure>
+        <img src={thumbnailUrl} />
+      </figure>
+      <div>
+        <span>Artist: {artist}</span>
+        <span>Price: {(price / 100).toFixed(2)}€</span>
+      </div>
+    </Link>
+  );
+};
+
 export default PicturePreview;
